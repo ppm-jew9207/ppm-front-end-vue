236f9d884572626c8de11cbc2d0f4a6b8a674e26
auris auris <aurimas.butk@gmail.com>
2020-06-04 18:00:46 +0100

    create number block for rendering page

---
+++ src/components/numberBlock.vue
@@ -0,0 +1,19 @@
+/<template>
+  <div class="overflow-auto">
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      aria-controls="my-table"
+    ></b-pagination></div>
+</template>
+
+<script>
+export default {
+    name:"numberBlock",
+}
+</script>
+
+<style>
+
+</style>
