9e25b406b392b5d69134273a744fea110d89b53a
Gardemarin <gintaras.kuskys@gmail.com>
2020-06-07 17:55:29 +0100

    Adds basic Ads & Ad pages.

---
+++ src/components/AdsPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <b-row tag="div" class="my-3">
+
+    <b-col tag="div" lg="6" v-for="n in 10" :key="n">
+      <app-ad></app-ad>
+    </b-col>  
+
+    <b-pagination
+        class="mx-auto"
+        v-model="currentPage"
+        :total-rows="rows"
+        :per-page="perPage"
+      ></b-pagination>
+  </b-row> 
+</template>
+
+<script>
+import appAd from '@/components/Ad';
+
+export default {
+  components: {
+    appAd 
+  },
+  data() {
+    return {
+      rows: 30,
+      perPage: 15,
+      currentPage: 5
+    };
+  },
+}
+</script>
