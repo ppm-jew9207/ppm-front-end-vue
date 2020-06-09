1709fd08f44e1f59fcf5a8c71a28a4c14cc0fe3a
LaimisMilas <laimis.milasius@gmail.com>
2020-06-09 22:57:19 +0300

    added vue-router and home page,login page

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <router-link to="/login">Prisijungti</router-link>
+    <router-link to="/register">Registruotis</router-link>
+  </div>
+</template>
+<style scope>
+
+    a{
+            color: black;
+            display: block;
+            width: 220px;
+            background-color: azure;
+            margin-left: auto;
+            margin-right: auto;
+            margin-top: 10px;
+            text-align: center;
+    }
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 // Install BootstrapVue
@@ -10,5 +11,6 @@ Vue.use(IconsPlugin);
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
